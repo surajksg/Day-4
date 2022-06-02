06a8ca6dfb113d6d36713c75c053404e2bb2999f
SURAJ <SURAJ@SRK007Suraj>
2022-06-02 22:06:09 +0530

    First commit

---
+++ day-04/src/demo.java
@@ -0,0 +1,13 @@
+
+public class demo {
+      int x;
+}
+class TestDemo{
+	public static void main(String[] args) 
+	{
+		demo myobj = new demo();
+		myobj.x = 40;
+        System.out.println(myobj.x);
+	}
+
+}
---
+++ day-04/src/demo2.java
@@ -0,0 +1,12 @@
+
+public class demo2 {
+   int x;
+   public static void main(String[] args) {
+	   demo2 myobj1 = new demo2();
+	   demo2 myobj2 = new demo2();
+	   myobj1.x = 24;
+	   myobj2.x = 55;
+	   System.out.println(myobj1.x);
+	   System.out.println(myobj2.x);
+   }
+}
